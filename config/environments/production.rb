114b694abf5102ac8730b57267e6fecad92ef15e
Prateek Gupta <prateek@prateekgupta.org>
2016-01-23 22:27:37 -0800

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # required for heroku
+  # note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'pg-pinteresting.com' }
+  
 end
